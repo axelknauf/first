b1c13e7f28f0921ff950f88171456a71d72444a1
Axel Knauf <axel.knauf@posteo.de>
2018-02-17 11:24:32 +0100

    Counting working, yay

--- src/first/core.clj
+++ src/first/core.clj
@@ -3,11 +3,32 @@
 
 (use 'clojure.java.io)
 
+(def collected-words (atom {}))
+
+(defn inc-word-using-key
+  "increments a single word count in the given map with key"
+  [m k]
+  (update-in m [k] (fnil inc 0)))
+
+(defn inc-word-count [word]
+  "updates the count for the given word in `collected-words`"
+  (swap! collected-words inc-word-using-key (keyword word)))
+
+(defn process-line
+  "counts the words in the given vector"
+  [words]
+  (doseq [word words]
+    (inc-word-count word)))
+
+(defn process-file
+  "reads the given file line by line and extracts the words"
+  [filename]
+  (with-open [rdr (reader filename)]
+    (doseq [line (line-seq rdr)]
+      (process-line (clojure.string/split line #"\ +")))))
+
 (defn -main
   "Read a file and produce a hit list of word counts"
   [& args]
-  (with-open [rdr (reader "short.txt")]
-    (doseq [line (line-seq rdr)]
-      (->> (clojure.string/split line #"\ +")
-           (println)))))
-
+  (process-file "short.txt") ; try some.txt
+  (println @collected-words))
